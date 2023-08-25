ecdb0f010cea6de82b375afeecfb381fff5bb4d3
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2023-08-26 02:55:15 +0530

    spell check

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,14 +15,14 @@ const Intro = () => {
       </Text>
       <Text style={styles.text}>
         Whenever you want to go back, press the back button! I know this sounds
-        silly! Lets say to left open the Mirror, when you come back it may
+        silly! Lets say you left open the Mirror, when you come back it may
         appear to be a black screen, just use the back button to go back and
         have a fresh start!
       </Text>
       <Text style={styles.text}>
-        Last but not least, this app doesn't store any data in of your data in
-        servers, everything is stored locally in your device! So, you can be
-        stress free! Stay safe!
+        Last but not least, this app doesn't store any of your data in servers,
+        everything is stored locally in your device! So, you can be stress free!
+        Stay safe!
       </Text>
     </View>
   );
